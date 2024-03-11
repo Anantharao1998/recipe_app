325de059a6b979e386ea88893b4c6ba0307471a9
anantharao <anantha@neurogine.com>
2024-03-11 10:04:51 +0800

    Added slide down animation

--- lib/core/extensions/animation_extension.dart
+++ lib/core/extensions/animation_extension.dart
@@ -4,17 +4,30 @@ import 'package:pokemondb/core/core.dart';
 extension AnimationExtension on Widget {
   /// Slide down animation
   Widget slideDown({
-    final Duration duration = const Duration(seconds: 5),
+    final Duration duration = const Duration(seconds: 2),
     final double begin = -100.0,
     final double end = 0.0,
   }) =>
-      _SlideTransitionWidget(child: this);
+      _SlideTransitionWidget(
+        animationDuration: duration,
+        begin: begin,
+        end: end,
+        child: this,
+      );
 }
 
 class _SlideTransitionWidget extends StatefulWidget {
-  const _SlideTransitionWidget({required this.child});
+  const _SlideTransitionWidget({
+    required this.child,
+    required this.animationDuration,
+    required this.begin,
+    required this.end,
+  });
 
+  final Duration animationDuration;
+  final double begin;
   final Widget child;
+  final double end;
 
   @override
   State<_SlideTransitionWidget> createState() => _SlideTransitionWidgetState();
@@ -23,17 +36,17 @@ class _SlideTransitionWidget extends StatefulWidget {
 class _SlideTransitionWidgetState extends State<_SlideTransitionWidget> with SingleTickerProviderStateMixin {
   /// Animation Controller variable
   late final AnimationController _controller = AnimationController(
-    duration: const Duration(seconds: 10),
+    duration: widget.animationDuration,
     vsync: this,
-  )..animateTo(1);
+  );
 
   late final Animation<Offset> _offsetAnimation = Tween<Offset>(
-    begin: Offset.zero,
-    end: const Offset(1.5, 0),
+    begin: Offset(0, widget.begin),
+    end: Offset(0, widget.end),
   ).animate(
     CurvedAnimation(
       parent: _controller,
-      curve: Curves.elasticIn,
+      curve: Curves.decelerate,
     ),
   );
 
@@ -41,6 +54,13 @@ class _SlideTransitionWidgetState extends State<_SlideTransitionWidget> with Sin
   void dispose() {
     _controller.dispose();
     super.dispose();
+  }
+
+  @override
+  void initState() {
+    _controller.animateTo(1);
+
+    super.initState();
   }
 
   @override
